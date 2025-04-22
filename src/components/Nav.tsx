c6faf1fa157a6968f16d92e31b9389fc41c29fb9
Andrew Birmingham <andrew_birmingham@msn.com>
2025-04-22 17:51:24 -0600

    added Home button to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
+  const currentPage = useLocation().pathname;
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>Nav
+      <h2>
+        <Link 
+        to ='/CandidateSearch'
+        className={
+          currentPage === '/WatchList' ? 'nav-link active' : 'nav-link'}
+        >
+        HOME
+        </Link>
+
+      </h2>
+    </div>
   )
 };
 
