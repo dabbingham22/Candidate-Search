ff43ec30f5c07d2e90aa1b1839f5a36252e0e13a
Andrew Birmingham <andrew_birmingham@msn.com>
2025-04-22 17:41:36 -0600

    Updated read me with needed info

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
-const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+const CandidateSearch = async () => {
+  const searched = await useEffect(searchGithub); //use effect to make searchGithub not a promise
+  return <div><h1>CandidateSearch</h1>
+      <h2>{searched}</h2>
+  </div>;
 };
 
 export default CandidateSearch;
